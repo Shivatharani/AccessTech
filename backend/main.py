44ca12a25e802b799c63ea635c51289f5d394bc9
Shivatharani <shivatharanimohan@gmail.com>
2026-03-29 11:51:12 +0530

    Deploy changes

--- backend/main.py
+++ backend/main.py
@@ -8,15 +8,23 @@ app = FastAPI(
     version="1.0"
 )
 
+import os
+
+allow_origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000"
+]
+
+frontend_url = os.getenv("FRONTEND_URL")
+if frontend_url:
+    allow_origins.append(frontend_url.rstrip("/"))
+
 # CORS FIX
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "http://localhost:3000",
-        "http://127.0.0.1:3000"
-    ],
+    allow_origins=allow_origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
